9c69aa41982dc5656faa8123e564195a24a5a248
xvasko <matej.vasko@outlook.com>
2017-11-06 17:02:03 +0100

    final commit

--- src/app/components/AlbumDetails.js
+++ src/app/components/AlbumDetails.js
@@ -1,12 +1,13 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Div } from './AlbumDetails.styles';
 
 export class AlbumDetails extends React.Component {
     render() {
 
         return (
-            <div>
+            <Div>
                 <img src={this.props.albumDetails.artworkUrl100} alt="album_cover"/>
                 <br/><br/>
                 <Link to={''} onClick={() => {
@@ -16,7 +17,7 @@ export class AlbumDetails extends React.Component {
                 }}>dislike</Link>
                 <br/><br/>
                 <Link to={''} onClick={() => this.props.hideAlbumDetails()}>back</Link>
-            </div>
+            </Div>
         );
     }
 }
---
+++ src/app/components/AlbumDetails.styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Div = styled.div`
+    a {
+        font-family: sans-serif;
+        margin-right: 264px;
+        text-decoration: none;
+        color: white;
+        font-weight: bold;
+    }
+`;
